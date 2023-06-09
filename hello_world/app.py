24e43df651443ac8ef0ad9b39bb44fa183f9354f
Ricardo Arian Ortega Garcia <40581565+rortega-sps@users.noreply.github.com>
2023-06-09 16:56:06 -0600

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello AWS",
+            "message": "Hello AWS",
             # "location": ip.text.replace("\n", "")
         }),
     }
